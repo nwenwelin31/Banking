d82ae8c9b9fe2899daaf82fd267e18ba8da1dd05
nwenwelin31 <155506534+nwenwelin31@users.noreply.github.com>
2024-04-08 14:59:17 +0630

    create user model

---
+++ src/main/java/com/banking/bankSecure/BankSecureApplication.java
@@ -0,0 +1,13 @@
+package com.banking.bankSecure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSecureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banking/bankSecure/BankSecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.bankSecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
